cb6daade08cfd807d725e4eaf94986d371b46f3f
robertolatini31 <robertolatini31@gmail.com>
2022-04-06 15:27:17 +0200

    adjusted

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,10 +7,10 @@
 // Visualizzare in pagina 5 numeri casuali.
 
 // collego l'elemnto html in cui inserisco i numeri
-const randomNumbersElement = document.querySelector('.random_numbers');
+randomNumbersElement = document.querySelector('.random_numbers');
 
 // creo un array di 5 numeri random ---- OK
-    const generatedNumbersArray = [];
+generatedNumbersArray = [];
     for (let i = 0; i < 5; i++) {
         generatedNumbersArray[i] = Math.floor(Math.random() * 10 + 1);
         // e li stampo a chermo
@@ -25,9 +25,9 @@ const randomNumbersElement = document.querySelector('.random_numbers');
 
 // Dopo 30 secondi l'utente deve inserire (uno alla volta) i numeri che ha visto precedentemente, tramite il prompt()
 // faccio pulire html dopo 29s
-setTimeout(deleteHtml, 2900)
+setTimeout(deleteHtml, 29000)
 // faccio inserire 5 numeri da prompt dopo 30s
-setTimeout(askNumbers(generatedNumbersArray, randomNumbersElement), 30000)
+setTimeout(askNumbers, 30000)
 
 // funzione che pulisce html
 function deleteHtml () {
@@ -35,17 +35,17 @@ function deleteHtml () {
 }
 
 //funzione che chiede 5 numeri
-function askNumbers(arrayGenerated, htmlElement) {
+function askNumbers () {
     const inserted = [];
     for(let i = 0; i < 5; i++) {
         inserted[i] = parseInt(prompt('inserisci un numero'));
         //console.log(inserted[i]);
     }
     for(let j = 0; j < 5; j++){ // ciclo che stampa a schermo i risultati
-        if(arrayGenerated[j] == inserted[j]){
-            htmlElement.innerHTML += `bravo era: ${inserted[j]}`;
+        if(generatedNumbersArray[j] == inserted[j]){
+            randomNumbersElement.innerHTML += ` bravo era: ${inserted[j]} `;
         } else  {
-            htmlElement.innerHTML += `hai sbagliato era: ${arrayGenerated[j]}`;
+            randomNumbersElement.innerHTML += ` hai sbagliato era: ${generatedNumbersArray[j]} `;
         }
     }
 
